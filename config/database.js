236b3096657f7624f0dd6948ea59370c14a89666
DmitriyBazilevskiy <dima17291195@gmail.com>
2022-06-02 16:30:05 +0300

    v 2

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dbgss.c6d4qw84ftau.eu-west-2.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'dbtest.czqa0vgsdw94.eu-west-2.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'dbgss'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'qwerty009'),
+      database: env('DATABASE_NAME', 'dbtest'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'rootpass'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
