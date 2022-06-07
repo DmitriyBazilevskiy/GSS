435db4d526fece5f1b708bc896b2aab463305a3c
DmitriyBazilevskiy <dima17291195@gmail.com>
2022-06-07 12:27:28 +0300

    APP_KEYS and token

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '1234567890qwe@232'),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', '987654321dfsa2'),
   },
 });
